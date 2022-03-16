f5117de9e37195d805101c6c9a3d98a1da25bd91
Ali_elesgerov <noid379@gmail.com>
2022-03-16 23:04:00 +0400

    Add project files.

---
+++ cs_Client/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace cs_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly Socket _client;
+        private readonly IPEndPoint _remoteEp;
+        private readonly byte[] _buffer;
+        private readonly Socket _listener;
+        private  EndPoint _endPoint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _client = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            const int port = 45678;
+            _remoteEp = new IPEndPoint(ip, port);
+
+             _buffer = Encoding.Default.GetBytes("Start");
+
+             _listener = new Socket(
+                 AddressFamily.InterNetwork,
+                 SocketType.Dgram,
+                 ProtocolType.Udp);
+
+             const int listenerPort = 45679;
+             var listenerEp = new IPEndPoint(ip, listenerPort);
+             _listener.Bind(listenerEp);
+
+             _endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        }
+
+        private void ButtonTakeScreenShot_Click(object sender, EventArgs e)
+        {
+            _client.SendTo(_buffer, _remoteEp);
+            ReceivePhoto();
+        }
+
+        private void PullImage(MemoryStream ms)
+        {
+            var imageSource = new BitmapImage();
+            imageSource.BeginInit();
+            imageSource.CacheOption = BitmapCacheOption.OnLoad;
+            imageSource.StreamSource = ms;
+            imageSource.EndInit();
+            image1.Source= imageSource;
+        }
+
+        private void ReceivePhoto()
+        {
+            var bytes = new byte[ushort.MaxValue - 28];
+
+            var len =_listener.ReceiveFrom(bytes, ref _endPoint);
+            using (var ms = new MemoryStream(bytes, 0, len))
+            {
+                PullImage(ms);
+            }
+
+        }
+    }
+}
+
---
+++ cs_ConsoleServer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace cs_ConsoleServer
+{
+    internal static class Program
+    {
+        private static Socket _client;
+        private static IPEndPoint _connectEp;
+
+        private static void Main()
+        {
+
+            #region Listener
+
+            var listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            const int port = 45678;
+
+            var endPoint = new IPEndPoint(ip, port);
+
+            listener.Bind(endPoint);
+
+            #endregion
+
+
+            #region Client
+
+            _client = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+            _connectEp = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45679);
+
+            #endregion
+
+            EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            var buffer= new byte[650000];
+
+            while (true)
+            {
+                var len = listener.ReceiveFrom(buffer, ref remoteEndPoint);
+                var message = Encoding.Default.GetString(buffer, 0, len);
+                if (message != "Start") continue;
+                Console.WriteLine("Message Received!");
+                SendImage();
+            }
+        }
+
+        private static Image ResizeImage(Image image) => new Bitmap(image, new Size(image.Width/4, image.Height/4));
+
+        private static void SendImage()
+        {
+            var image = TakeScreenShot();
+            var imageBytes = ConvertToByteArray(ResizeImage(image));
+
+            _client.SendTo(imageBytes,_connectEp);
+
+        }
+        private static Image TakeScreenShot()
+        {
+            Console.WriteLine("Screen captured");
+            return PrintScreen.CaptureScreen();
+        }
+
+        private static byte[] ConvertToByteArray(Image image)
+        {
+            var imageConverter = new ImageConverter();
+            return (byte[])imageConverter.ConvertTo(image, typeof(byte[]))!;
+        }
+
+    }
+}
